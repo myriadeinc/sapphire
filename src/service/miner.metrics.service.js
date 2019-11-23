a9df98b0d6f58d2f180ebd60c8a2390ed903c11f
Han Xiao <yhxiao97@gmail.com>
2019-11-23 16:00:22 -0500

    ready for hashrate calculation

--- src/service/miner.metrics.service.js
+++ src/service/miner.metrics.service.js
@@ -6,26 +6,29 @@ const mq = require('src/util/mq.js');
 
 const logger = require('src/util/logger.js').core;
 
+const BLOCK_REWARD = 0.3;
+
 const MinerMetricsService = {
 
-  convertShareToHashrate: (shares, difficulty) => {
+  convertShareToHashrate: (shares, difficulty, timestamp, minerId) => {
     return 100;
   },
 
-  computeAward: (hashrate) => {
-    return 1;
+  computeAward: (hashrate, network_hashrate) => {
+    return (hashrate / network_hashrate) * 0.9 * (10**7) * BLOCK_REWARD;s
   },
 
   processData: async (data) => {
     try{
       let miner = await MinerRepository.getMiner(data.minerId);
-      let hashrate = MinerMetricsService.convertShareToHashrate(data.shares, data.difficulty);
+      let hashrate = MinerMetricsService.convertShareToHashrate(data.shares, data.difficulty, data.timestamp, data.minerId);
+      let network_hashrate = 100; // Change this
       await MinerRepository.updateHashrate({
         minerId: miner.id,
         rate: hashrate,
         time: data.timestamp
       })
-      new_myriade_credits = MinerMetricsService.computeAward(hashrate);
+      new_myriade_credits = MinerMetricsService.computeAward(hashrate, network_hashrate);
       if (new_myriade_credits > 0){
         await MinerRepository.updateMiner({
           minerId: miner.id,
