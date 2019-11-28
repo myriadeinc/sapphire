d4db0d8aeecd4e4d5ae939a904188d2ff714932e
andyjung-dev <andy_jng@outlook.com>
2019-11-27 22:02:21 -0500

    [sapphire] Issue #8: feat: Add base code for constantBasePayout

--- src/service/payout.service.js
+++ src/service/payout.service.js
@@ -16,7 +16,7 @@ const PayOutService = {
 
     perBlockPayout: async () => {
 
-        const now = Date.now();
+    const now = Date.now();
 
     const miners = await MinerRepository.getAllMiners();
     let poolHashrate = 0;
@@ -54,9 +54,53 @@ const PayOutService = {
 
     },
     /**
-     * Calculate reward for each miner
+     * Calculate reward for each miner based on set fee
      */
     constantPayout: async () => {
+        let networkHashrate = this.getNetworkHashrate();
+        let blockReward = this.getBaseBlockreward();
+
+        await DB.sequelize.transaction(async (t) => {
+            await Promise.all(miners.map( async (miner) => {
+                let minerShares = await MinerRepository.getUncalculatedShares(miner.id);
+                const last = minerShares[minerShares.length -1].time;
+                let minerHashrate = _.reduce(minerShares, (acc, share) => {
+                    return acc + ( (share.share * share.difficulty) / (now - last) );
+                  }, 0);
+
+
+            // Updating Miner Hashrate
+            await HashRateModel.create({
+                minerId: miner.id,
+                rate: minerHashrate,
+                time: now,
+              }, {transaction: t});
+
+            let credit = minerHashrate / networkHashrate * 10**7 * 0.9 * blockReward;
+            await CreditModel.create({
+                minerId: miner.id,
+                credit: credit,
+
+
+                time: now
+            });
+            // Updates every shares to be is_calculated
+            await Promise.all(minerShares.map(share => {
+              return share.update({
+                is_calculated: true
+              }, {transaction: t});
+            }))
+
+            await Promise.all(miners.map(async (miner) => {
+                return miner.cre
+
+
+
+                }
+            ));
+
+            }));
+        });
 
 
 
---
+++ test/services/payout.service.test.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const _ = require('lodash');
+
+const faker = require('faker');
+
+const testing = require('../test.init.js');
+
+const MinerTestingHelper = require('test/helpers/miner.helper.js');
+const ShareTestingHelper = require('test/helpers/shares.helper.js');
+
+const PAyoutService = require('src/service/payout.service.js');
+const MinerRepository = require('src/repository/miner.repository.js');
+
+
+const HashrateModel = require('src/models/hashrate.model.js');
+const CreditModel = require('src/models/credit.model.js');
+const ShareModel = require('src/models/share.model.js');
+
+
+require('chai')
+  .use(require('chai-as-promised'))
+  .use(require('chai-string'))
+  .should();
+
+  describe('Miner Payout Service Tests', () => {
+    before('Await DB', () => {
+      return testing.dbReady;
+    });
+  
+    before('Setting up miners', async () => {
+      await MinerTestingHelper.clearAllMiners();
+      await MinerTestingHelper.createSampleMiners();
+  
+      await MinerTestingHelper.addSampleHashrates(MinerTestingHelper.minerId_1);
+      await MinerTestingHelper.addSampleCredits(MinerTestingHelper.minerId_1);
+    })
+  
+    after('Cleanup', async () => {
+      await MinerTestingHelper.clearAllMiners();
+    })
+  
+    it('Should give each miner a constant payout', async () => {
+
+        const trigger = await PayoutService.constantPayout();
+
+
+
+      const miner = await MinerRepository.getMinerDataById(MinerTestingHelper.minerId_1);
+      miner.should.not.be.null;
+      miner.monero_balance.should.equal('0');
+    })
+  
+  })
