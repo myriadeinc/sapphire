b1e3a200d071d805d3627ed1a2f440d960300b8e
Jonathan <jonathanxu25@gmail.com>
2020-05-05 16:30:37 -0400

    testing circleci

--- src/util/logger.js
+++ src/util/logger.js
@@ -1,33 +1,33 @@
 'use strict';
 const bunyan = require('bunyan')
-, bformat = require('bunyan-formatter')  
-, formatOut = bformat({ outputMode: 'short', level: 'debug'});
+  , bformat = require('bunyan-formatter')
+  , formatOut = bformat({ outputMode: 'short', level: 'debug' });
 
 const config = require('src/util/config.js');
 
-let LogDNAStream = require('logdna-bunyan').BunyanStream;
-let logDNA = new LogDNAStream({
-  key: config.get('log:logdna_api_token')
-});
+// let LogDNAStream = require('logdna-bunyan').BunyanStream;
+// let logDNA = new LogDNAStream({
+//   key: config.get('log:logdna_api_token')
+// });
 
 const logger = bunyan.createLogger({
   name: 'sapphire',
-  streams: [ 
+  streams: [
     {
-      stream:formatOut 
-    }, 
+      stream: formatOut
+    },
     {
-      stream: logDNA,
+      // stream: logDNA,
       type: 'raw'
     }
   ]
 });
 
 module.exports = {
-  core: logger.child({component: 'core'}),
-  account: logger.child({component: 'account'}),
-  db: logger.child({component: 'db'}),
-  mq: logger.child({component: 'mq'}),
-  job: logger.child({component: 'job'}),
-  minerRepository: logger.child({component: 'minerRepository'}),
+  core: logger.child({ component: 'core' }),
+  account: logger.child({ component: 'account' }),
+  db: logger.child({ component: 'db' }),
+  mq: logger.child({ component: 'mq' }),
+  job: logger.child({ component: 'job' }),
+  minerRepository: logger.child({ component: 'minerRepository' }),
 };
