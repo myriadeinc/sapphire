45f0c56d7ff2abd6a28853c6ba51843576e38206
andyjung-dev <andy_jng@outlook.com>
2020-06-24 17:05:54 -0400

    Update test

--- test/services/miner.metrics.service.test.js
+++ test/services/miner.metrics.service.test.js
@@ -95,7 +95,7 @@ describe("Miner Metrics Service Functional Tests", () => {
             blockHeight: 0
         }
         const result = await MinerMetricsService.processData(data);
-        result.shoud.not.be.null;
+        result.should.not.be.null;
 
 
     });
