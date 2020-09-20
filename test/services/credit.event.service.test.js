3330dca6f94608f8cc34216f1f5a295037bb2147
rifathanano <rifat@myriade.io>
2020-09-20 17:31:38 -0400

    ALPHA-1 Create Sapphire endpoints for lilkit

--- test/services/credit.event.service.test.js
+++ test/services/credit.event.service.test.js
@@ -6,14 +6,15 @@ const testing = require("../test.init.js");
 const MinerTestingHelper = require("test/helpers/miner.helper.js");
 const CreditEventService = require("src/service/credit.event.service.js");
 const MinerRepository = require("src/repository/miner.repository.js");
-
+const EventCmsModel = require("../../src/models/event.cms.model.js");
+let eventInfo;
 
 require("chai")
     .use(require("chai-as-promised"))
     .use(require("chai-string"))
     .should();
 
-describe("Credit Event Service Unit Tests", () => {
+describe.only("Credit Event Service Unit Tests", () => {
     before("Await DB", () => {
         return testing.dbReady;
     });
@@ -21,11 +22,13 @@ describe("Credit Event Service Unit Tests", () => {
     before("Setting up miners", async () => {
         await MinerTestingHelper.clearAllMiners();
         await MinerTestingHelper.createSampleMiners();
-    });
+        eventInfo = await CreditEventService.createEvent({ prizeAmount: 100 })
 
+    })
     after("Cleanup", async () => {
         await MinerTestingHelper.clearAllMiners();
-    });
+        await EventCmsModel.destroy({ where: { id: eventInfo.id } })
+    })
 
     it("Should be able to lock a miners funds", async () => {
         const minerId = MinerTestingHelper.minerId_1;
@@ -33,12 +36,12 @@ describe("Credit Event Service Unit Tests", () => {
         const amount = 1000;
         const lockType = 1;
 
-        await CreditEventService.create(minerId, amount, lockType);
+        await CreditEventService.create(minerId, amount, lockType, eventInfo.id);
         const miner = await MinerRepository.getMinerDataById(minerId);
-        const newBalance = miner.credits;
+        const newBalance = miner.credits.toString()
 
-        newBalance.should.equal(9000);
-    });
+        newBalance.should.equal('9000');
+    })
 
     //   it("Should be able to fully release funds", async () => {
     //     // miner2 add lock funds equal to monero balance
@@ -47,5 +50,21 @@ describe("Credit Event Service Unit Tests", () => {
     //     // miner2 balance should be all empty
     //   });
 
-    //   it("Should be able to do some stuff", async () => { });
+    it("Should mark an event as closed", async () => { })
+    it("Should fetch all participants in a credit event", async () => {
+        // This gives all miners some credits
+        await Promise.all(MinerTestingHelper.sampleMiners.map(miner => CreditEventService.grantMinerCredits(miner.id, 100)))
+        // This enrolls all miners to a credit event
+        await Promise.all(MinerTestingHelper.sampleMiners.map(miner => CreditEventService.create(miner.id, 10, 1, eventInfo.id)))
+
+        // One extra credit event
+        await CreditEventService.create(MinerTestingHelper.minerId_1, 10, 1, eventInfo.id)
+        // Fetch all miners, duplicate ones should be combined into one entry
+        const participants = await CreditEventService.getUniqueParticipants(eventInfo.id)
+        const expected = MinerTestingHelper.sampleMiners.map(miner => miner.id.toString())
+        participants.should.eql(expected)
+
+
+    })
+
 });
