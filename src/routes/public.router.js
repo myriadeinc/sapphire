0cf046247874d2cb92c36049bca6d76b05f73099
Rifat Hanano <rifat@myriade.io>
2021-02-01 17:40:04 -0500

    Add pre beta changes

---
+++ src/routes/public.router.js
@@ -0,0 +1,37 @@
+"use strict";
+/*eslint-disable */
+const router = require("express").Router();
+/* eslint-enable */
+const MinerMetricsService = require("src/service/miner.metrics.service.js");
+const SystemHashrateModel = require("src/models/system.hashrate.model.js");
+const ShareModel = require("src/models/share.model.js");
+
+const logger = require("src/util/logger.js").db;
+
+router.use(AuthMiddleware.validateMinerId);
+
+router.get("/poolInfo",
+    async (req, res) => {
+        try {
+            const currHeight = MinerMetricsService.currentHeight.blockHeight;
+            const rate = await SystemHashrateModel.findByPk(currHeight.toString());
+            const nminers = await ShareModel.count({
+                distinct: true,
+                col: "minerId",
+                
+                where: {
+                blockHeight: currHeight,
+            }});
+            return res.status(200).send({
+                "hashrate": rate,
+                "miners": nminers
+            });
+        }
+        catch (e) {
+            logger.error(e);
+            return res.status(500).send({ error: `Unable to fetch minerId: ${minerId}`, code: 500 });
+        }
+    });
+
+
+module.exports = router;
