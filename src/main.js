98c9108df704a314bda64e349a764cf7b0cf32d3
Rifat Hanano <rifat@myriade.io>
2021-03-13 22:07:23 -0500

    BETA-2 BigInt

--- src/main.js
+++ src/main.js
@@ -34,7 +34,7 @@ const main = async () => {
 
     logger.core.info("Registering MinerMetrics service listener");
     let blockHeight = await MoneroApi.getInfo();
-    blockHeight = blockHeight.height;
+    blockHeight = BigInt(blockHeight.height);
     await require("src/service/miner.metrics.service.js").init(blockHeight);
     logger.core.info(`MinerMetrics service listener registered with blockHeight ${blockHeight}`);
   });
